77e1e7a8922511e78b784ec627ef941c9c26552e
Ahmet Özmen <ozmen@sakarya.edu.tr>
2024-04-17 14:17:06 +0300

    Initial commit.

---
+++ src/test/java/org/sau/ghwf/GhwfApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sau.ghwf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhwfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
